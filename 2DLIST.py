cf1f1337d79ab49360768b93b3a2fffc469c9184
kirey1412 <kireykayana14@gmail.com>
2024-08-15 20:06:35 +0700

    Add files via upload

---
+++ 2DLIST.py
@@ -0,0 +1,22 @@
+matrix=[[1,3],[5,6],[4,1]]
+print(matrix)
+#Number Of Rows
+print(len(matrix))
+
+#Number of Columns
+print(len(matrix[0]))
+
+print(matrix[1][0])
+
+
+matrix1=[[3,4,5],[7,8,9],[1,2,3]]
+
+#Number of Rows
+print(len(matrix1))
+
+#Number of Columns
+print(len(matrix1[1]))
+
+for i in range(0,len(matrix1)):
+    for g in range(0,len(matrix1[2])):
+        pass
